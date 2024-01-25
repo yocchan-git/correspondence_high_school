b9ff775fad27f9cdc91155c581644b8735b73703
yocchan-git <yocchan.business@gmail.com>
2024-01-25 14:32:18 +0900

    chs.jsファイルを作成する

---
+++ chs.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+import enquirer from "enquirer";
+import fsPromises from "fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+selectStories(__dirname);
+
+async function selectStories(__dirname) {
+  console.log(
+    "このアプリでは,5つの質問であなたの通信制高校への適応度を測ります\n",
+  );
+  await timeOut(1500);
+  console.log("ただ、最終的な意思決定はご自身で行なってくださいね！\n");
+  await timeOut(1500);
+  console.log("それでは、スタート\n");
+  await timeOut(1500);
+
+  const questions = JSON.parse(
+    await fsPromises.readFile(`${__dirname}/question.json`),
+  );
+  const totalPoints = await appropriateQuestion(questions, __dirname);
+
+  const fitnessMessage = `あなたの適応度は${totalPoints}%です`;
+  const comment = await createComment(totalPoints);
+
+  console.log(fitnessMessage);
+  console.log(comment);
+}
+
+async function timeOut(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function appropriateQuestion(questions, __dirname) {
+  let percentPoints = 0;
+
+  for (let i = 0; i <= 4; i++) {
+    percentPoints += await questionDetails(questions[i], __dirname);
+  }
+
+  return percentPoints;
+}
+
+async function questionDetails(question, __dirname) {
+  const choiceAnswers = JSON.parse(
+    await fsPromises.readFile(`${__dirname}/${question.detailsFile}`),
+  );
+
+  const query = {
+    type: "select",
+    name: "point",
+    message: question.title,
+    choices: choiceAnswers,
+    result() {
+      return this.focused.point;
+    },
+  };
+
+  const answer = await enquirer.prompt(query);
+
+  if (answer.point === "") {
+    return 0;
+  }
+  return answer.point;
+}
+
+async function createComment(totalPoints) {
+  return new Promise((resolve) => {
+    if (totalPoints >= 75) {
+      resolve("通信制高校に向いてるかも！前向きに検討してみてね");
+    } else if (totalPoints < 75 && totalPoints >= 50) {
+      resolve("通信制高校にちょい向いてる。資料請求してみたらいいかも");
+    } else if (totalPoints < 50 && totalPoints >= 25) {
+      resolve(
+        "通信制高校にちょい向いてなさそう。今の環境で満足できるまで頑張ってみるといいかも",
+      );
+    } else {
+      resolve("通信制高校には向いてなさそう。今のまま楽しんで！");
+    }
+  });
+}
